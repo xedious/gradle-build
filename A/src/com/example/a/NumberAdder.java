c83bfc685ff146093b3708ce4e1d10b37e2b61ec
Jerry <jerry@electronicmarker.com>
2014-08-27 10:22:11 -0500

    initial commit

---
+++ A/src/com/example/a/NumberAdder.java
@@ -0,0 +1,7 @@
+package com.example.a;
+
+public class NumberAdder {
+	public static int addNumbers(int one, int two) {
+		return one + two;
+	}
+}
---
+++ B/src/com/example/b/test.java
@@ -0,0 +1,9 @@
+package com.example.b;
+
+import com.example.a.NumberAdder;
+
+public class test {
+	public int testNumberAdder() {
+		return NumberAdder.addNumbers(2,3);		
+	}
+}
